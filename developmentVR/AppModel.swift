8f01fcff64ea89d08b8f68acbf4aa4f5fb900b47
yosuahres <haresyosuaa@gmail.com>
2025-08-19 13:10:39 +0700

    Initial Commit

---
+++ developmentVR/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  developmentVR
+//
+//  Created by HARES on 8/19/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
